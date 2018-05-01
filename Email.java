7cd2d1da800cb9e389a39cb72136071f98779c45
Arvind Ravi <ar2au@virginia.edu>
2018-04-30 23:23:27 -0400

    Add Email class

---
+++ Email.java
@@ -0,0 +1,15 @@
+public class Email {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail;
+
+    // Constructor to receive first name and last name
+    // Ask for department
+    // Generate random password
+    // Set mailbox capacity
+    // Set alternate email
+    // Change the password
+}
